7cf6eb77e7f8082a130b5f9fba9319fc287ca317
MarianaRdz <42949838+MarianaRdz@users.noreply.github.com>
2018-09-24 17:41:39 -0500

    PRACTICA#3

---
+++ Registro(Servlet).java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Mariana Rodriguez Gonzalez 1809752
+ */
+@WebServlet(urlPatterns = {"/Registro"})
+public class Registro extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+                String nombre = request.getParameter("nombre");
+                String PrimerApellido = request.getParameter("PrimerApellido");
+                String SegundoApellido = request.getParameter("SegundoApellido");
+                String mes = request.getParameter("mes");
+                String correo = request.getParameter("correo");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Registrado</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1> Bienvenido! " + "</h1>");
+            out.println("¡Hola!, tu nombre es: " + request.getParameter("nombre") + request.getParameter("PrimerApellido") + request.getParameter("SegundoApellido"));
+            out.println("Tu correo es: " + request.getParameter("correo"));
+            out.println("Tu mes de nacimiento es:" + request.getParameter("mes"));
+            out.println("Gracias por Registrarte");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
